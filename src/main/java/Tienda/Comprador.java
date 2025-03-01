1764c0527672742f313482d2197f52ada0ae80c1
AnimaKevin10 <kflorezh@unal.edu.co>
2025-02-28 21:31:19 -0500

    actualizacion

--- src/main/java/Tienda/Comprador.java
+++ src/main/java/Tienda/Comprador.java
@@ -1,23 +1,23 @@
 package Tienda;
 
 public class Comprador extends Usuario {
-    private Carrito carrito;
+    private CarroDeCompras carrito;
 
     public Comprador(String nombre, String direccion, String numeroTarjeta, String telefono) {
         super(nombre, direccion, numeroTarjeta, telefono);
-        this.carrito = new Carrito();  // Inicializa el carrito siempre
+        this.carrito = new CarroDeCompras();  // Inicializa el carrito siempre
     }
     // Constructor para registro con autenticación (id, nombre, correo, contraseña, fecha de nacimiento)
     public Comprador(int idUsuario, String nombre, String correo, String contrasena, String fechaNacimiento) {
         super(idUsuario, nombre, correo, contrasena, fechaNacimiento);
-        this.carrito = new Carrito();  // Inicializa el carrito siempre
+        this.carrito = new CarroDeCompras();  // Inicializa el carrito siempre
     }
     //  Métodos Específicos
-    public Carrito getCarrito() {
+    public CarroDeCompras getCarrito() {
         return this.carrito;
     }
 
-    public void setCarrito(Carrito carrito) {
+    public void setCarrito(CarroDeCompras carrito) {
         this.carrito = carrito;
     }
 }
