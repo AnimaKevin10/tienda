2f5178ce9e62014d506699f9e8bf45923c8081f5
AnimaKevin10 <kflorezh@unal.edu.co>
2025-03-01 23:11:50 -0500

    Pantalla Login funcional

--- src/main/java/Tienda/Vista/VistaVendedor.java
+++ src/main/java/Tienda/Vista/VistaVendedor.java
@@ -70,10 +70,8 @@ public class VistaVendedor extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new VistaVendedor().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new VistaVendedor().setVisible(true);
         });
     }
 
