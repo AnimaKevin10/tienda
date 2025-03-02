d82010037311afd0895abd4814ab3867b90dcddd
DanielPamplona-UNAL <68169167+DanielPamplona-UNAL@users.noreply.github.com>
2025-03-02 16:36:31 -0500

    Add files via upload

---
+++ Videojuego.java
@@ -0,0 +1,37 @@
+package Tienda.Modelo;
+
+public class Videojuego extends Producto {
+    private String genero;
+    private String desarrollador;
+
+    // Constructor
+    public Videojuego(String titulo, String sku, double precio, int stock, String genero, String desarrollador) {
+        super(titulo, sku, precio, stock); // Llama al constructor de la clase base (Producto)
+        this.genero = genero;
+        this.desarrollador = desarrollador;
+    }
+
+    // ------------------ Getters ------------------
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getDesarrollador() {
+        return desarrollador;
+    }
+
+    // ------------------ Setters ------------------
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setDesarrollador(String desarrollador) {
+        this.desarrollador = desarrollador;
+    }
+
+    // Sobrescribir el método toString()
+    @Override
+    public String toString() {
+        return super.toString() + " | Género: " + genero + " | Desarrollador: " + desarrollador;
+    }
+}
